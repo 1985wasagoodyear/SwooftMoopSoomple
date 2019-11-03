32e238c5b06e0d9d4e611c43bd9c9a86c9e526f2
1985wasagoodyear <1985wasagoodyear>
2019-11-03 18:28:55 -0500

    Initial Commit

---
+++ SwooftMoopSoomple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwooftMoopSoomple
+//
+//  Created by K Y on 11/3/19.
+//  Copyright © 2019 K Y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
